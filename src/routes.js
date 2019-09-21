64a1125b5b362e4636a54ad6a1b7be3e673c6b8c
LucasManciniDaSilva <lucasmancini1997@gmail.com>
2019-09-21 15:50:04 -0300

    Criado ScheduleController e NotificationController

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,30 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
-
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
