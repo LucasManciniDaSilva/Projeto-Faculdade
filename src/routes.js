1b6889da1087de062a96244a4cdf6427cf2d1533
jsmiranda1 <54691115+jsmiranda1@users.noreply.github.com>
2019-09-21 17:33:26 -0300

    Ajustes e criações no Backend
    Criacão de AppointmentController e SessionController;
    Arquivo de configuração de sentry;
    Criação de layout de email de cancelamento;
    Alterações no routes.js;
    Criação de Queue.js no src;

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
-
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
-import authMiddleware from './app/middleware/auth';
-import authMiddleware from './app/middleware/auth';
-import ProviderController from './app/controllers/ProviderController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,12 +22,16 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
-
-routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
