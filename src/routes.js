dba60ea5982b9281b7e1d6dd3df4074bcbbe0d50
LucasManciniDaSilva <47194608+LucasManciniDaSilva@users.noreply.github.com>
2019-09-21 16:23:52 -0300

    Ajustes em Routes

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,9 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import authMiddleware from './app/middleware/auth';
 import authMiddleware from './app/middleware/auth';
 import ProviderController from './app/controllers/ProviderController';
 
@@ -18,6 +21,11 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.get('/providers', ProviderController.index);
 
