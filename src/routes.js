602c16a770360d5c06e2fd0ef2027e6139fb85e7
LucasManciniDaSilva <lucasmancini1997@gmail.com>
2019-09-21 16:08:20 -0300

    Ajustes no Routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,9 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 
-<<<<<<< HEAD
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
 import authMiddleware from './app/middleware/auth';
-=======
-import authMiddleware from './app/middleware/auth';
-import ProviderController from './app/controllers/ProviderController';
->>>>>>> c76521b8371aa769f276dd520965374cf7c5ceb3
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -25,14 +20,12 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-<<<<<<< HEAD
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
-=======
+
 routes.get('/providers', ProviderController.index);
->>>>>>> c76521b8371aa769f276dd520965374cf7c5ceb3
 
 routes.post('/files', upload.single('file'), FileController.store);
 
